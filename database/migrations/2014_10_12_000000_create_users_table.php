04531f5583c61533cdb1aae0648820e6abd5d2db
Joy Biswas <contact.joybiswas@gmail.com>
2023-08-20 16:34:44 +0600

    change in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->numeric('phone');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('role', UserRole::toArray())->default(UserRole::user);
