265455342cbc0d3c1a6882ea60702400884ef85c
Joy Biswas <contact.joybiswas@gmail.com>
2023-08-23 10:08:10 +0600

    new update on navigation

--- resources/views/layouts/gust_navigation.blade.php
+++ resources/views/layouts/gust_navigation.blade.php
@@ -1,9 +1,9 @@
 <div class="sm:fixed bg-gray-900 text-white sm:top-0 sm:right-0 p-6 text-right z-10">
     <a href="{{ route('login') }}"
-        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+        class="font-semibold text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
         in</a>
 
     <a href="{{ route('register') }}"
-        class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+        class="ml-4 font-semibold text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
 </div>
 <x-flash-message></x-flash-message>
